8e5859d714cbd709f24a49c8a0caa4f74b9d21cb
James Hillyerd <james@hillyerd.com>
2020-11-16 21:29:54 -0800

    qemu exit codes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // don't link std library
 #![no_main] // disable normal entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -8,13 +11,50 @@ mod vga_buffer;
 /// The linker entry point.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello macro World{}", "!");
+    println!("** blog_os starting **");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
 /// Called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
