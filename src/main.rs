8438469c2079a1db921d94546a184d7a5f380ae5
James Hillyerd <james@hillyerd.com>
2020-11-20 20:25:56 -0800

    improve test output

--- src/main.rs
+++ src/main.rs
@@ -54,19 +54,32 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
-
+    serial_println!("All tests pass.");
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 2);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
