0b5e207f5bf4d4ba3263eb70220cae49e407dd41
James Hillyerd <james@hillyerd.com>
2020-12-06 18:48:42 -0800

    add async keyboard scanner

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 extern crate alloc;
 
-use blog_os::task::{simple_executor::SimpleExecutor, Task};
+use blog_os::task::{keyboard::print_keypresses, simple_executor::SimpleExecutor, Task};
 use blog_os::{memory, memory::BootInfoFrameAllocator, println};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
@@ -36,6 +36,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // Async.
     let mut executor = SimpleExecutor::new();
     executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(print_keypresses()));
     executor.run();
 
     #[cfg(test)]
